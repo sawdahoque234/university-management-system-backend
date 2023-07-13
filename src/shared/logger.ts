18898a1e1b8d0b4c62224e9afa1ff8750f145074
sawda hoque <sawdahoque6066@gmail.com>
2023-07-13 11:19:34 +0600

    [done] customize logger

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,20 +1,39 @@
 import { createLogger, format, transports } from 'winston'
 const { combine, timestamp, label, prettyPrint } = format
+import DailyRotateFile from 'winston-daily-rotate-file'
 
 import path from 'path'
+
+// const myFormat = printf(({ level, message, label, timestamp }) => {
+//   const date = new Date(timestamp)
+//   const hour = date.getHours()
+//   const minutes = date.getMinutes()
+//   const seconds = date.getSeconds()
+//   return `${date.toDateString()} ${hour}:${minutes}:${seconds} } [${label}] ${level}: ${message}`
+// })
 
 const logger = createLogger({
   level: 'info',
   format: combine(
     label({ label: 'Logger-Status' }),
     timestamp(),
+    // myFormat()
     prettyPrint(),
   ),
   transports: [
     new transports.Console(),
-    new transports.File({
-      filename: path.join(process.cwd(), 'logs', 'winston', 'success.log'),
-      level: 'info',
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'successess',
+        'ums-%DATE%-success.log',
+      ),
+      datePattern: 'YYYY-MM-DD-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
     }),
   ],
 })
@@ -23,16 +42,27 @@ const errorlogger = createLogger({
   format: combine(
     label({ label: 'Logger-Status' }),
     timestamp(),
+    // myFormat()
     prettyPrint(),
   ),
   transports: [
     new transports.Console(),
-    new transports.File({
-      filename: path.join(process.cwd(), 'logs', 'winston', 'error.log'),
-      level: 'error',
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'errors',
+        'ums-%DATE%-error.log',
+      ),
+      datePattern: 'YYYY-MM-DD-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
     }),
   ],
 })
+
 export { logger, errorlogger }
 // error: 0
 // warn: 1
